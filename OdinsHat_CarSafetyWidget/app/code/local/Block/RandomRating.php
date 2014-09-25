70c7c2af288d465d4d2e034b67cdc67976342730
Doug Bromley <doug.bromley@gmail.com>
2014-09-25 18:54:08 +0100

    Adding basic block for random rating

---
+++ OdinsHat_CarSafetyWidget/app/code/local/Block/RandomRating.php
@@ -0,0 +1,11 @@
+<?php
+
+class OdinsHat_CarSafetyWidget_Block_RandomRating
+    extends Mage_Core_Block_Abstract
+    implements Mage_Widget_Block_Interface
+{
+    protected function _toHtml()
+    {
+        return 'Random Car Safety Rating ***';
+    }
+}
